4f6e0cc6356099d9f191d07811bb4d0e387f8d95
Jim Shaw <jimxshaw@gmail.com>
2017-03-05 16:17:19 -0600

    Updated project.json and added db context class to Core Data project.

---
+++ SamuraiAppCore/src/SamuraiAppCore.Data/SamuraiContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiAppCore.Domain;
+
+namespace SamuraiAppCore.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=OWNER-PC\\SQLEXPRESS;Database=SamuraiCoreData;Trusted_Connection=True;");
+        }
+    }
+}
